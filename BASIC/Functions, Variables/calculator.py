a75d905df6111476d0446fcaae47ac374d35f7e4
Gugo-le <rngustmd0719@naver.com>
2024-07-29 17:23:11 +0900

    n*n

--- BASIC/Functions, Variables/calculator.py
+++ BASIC/Functions, Variables/calculator.py
@@ -1,5 +1,8 @@
-x = int(input("What's x"))
-y = int(input("What's y"))
-
-z = round(x+y)
-print(z)
+def main():
+    x = int(input("What's x?"))
+    print("x squared is", square(x))
+    
+def square(n):
+    return n * n    
+    
+main()    
