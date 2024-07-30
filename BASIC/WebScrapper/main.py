177c29eee33ac946b49fa8d86f5ba3a383dcd148
Gugo-le <rngustmd0719@naver.com>
2024-07-30 21:58:22 +0900

    pagination

--- BASIC/WebScrapper/main.py
+++ BASIC/WebScrapper/main.py
@@ -1,21 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs#job-listings"
-
-response = requests.get(url)
-
-soup = BeautifulSoup(response.content, "html.parser")
-
-jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
-
 all_jobs = []
 
-for job in jobs:
-    title = job.find("span", class_="title").text if job.find(
+def scrape_page(url):
+    print(f"Scrapping {url}...")
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+
+
+    for job in jobs:
+        title = job.find("span", class_="title").text if job.find(
         "span", class_="title") else "Title not found"
 
-    # company, position, region = job.find_all("span", class_="company")
 
     spans = job.find_all("span", class_="company")
 
@@ -27,12 +26,6 @@ for job in jobs:
     # tooltip--flag-logo 다음 것(a)을 찾아 href를 추출함
     url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"]
 
-    '''
-    url을 찾지 못하는 경우
-    if url:
-        url=url["href"]
-    '''
-
     job_data = {
         "title": title,
         "company": spans[0].text,
@@ -43,5 +36,15 @@ for job in jobs:
 
     all_jobs.append(job_data)
 
+def get_pages(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    return len(soup.find("div", class_="pagination").find_all("span", class_="page"))
 
-print(all_jobs)
+total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
+
+for x in range(total_pages):
+    url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
+    scrape_page(url)
+    
+print(len(all_jobs))    
