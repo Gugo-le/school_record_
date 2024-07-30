fc17ebe376614e4131cad5d3a444ca9431f94bdc
Gugo-le <rngustmd0719@naver.com>
2024-07-30 15:11:44 +0900

    RECAPITULATION

---
+++ BASIC/Loops/web.py
@@ -0,0 +1,24 @@
+from requests import get
+
+websites = (
+    "google.com",
+    "airbnb.com",
+    "twitter.com",
+    "facebook.com"
+)
+
+
+results = {}
+
+for website in websites:
+    if not website.startswith("https://"):
+        website = f"https://{website}"
+    response = get(website)
+    if response.status_code == 200:
+        results[website] = "OK"
+       
+    else:
+        results[website] = "FAILED"
+        
+print(results)        
+        
