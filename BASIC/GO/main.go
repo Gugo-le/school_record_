46743cab3fb5842135c9f35e791be2ad163606b3
Gugo-le <rngustmd0719@naver.com>
2024-08-07 21:49:13 +0900

    naked return

--- BASIC/GO/main.go
+++ BASIC/GO/main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (length int, uppercase string) {
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	repeatMe("kevin", "ian", "rucas", "sophia", "andreaha")
+	totalLength, upperName := lenAndUpper("kevin")
+	fmt.Println(totalLength, upperName)
+
 }
