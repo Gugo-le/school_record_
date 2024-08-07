815b57cca880c46f557bb77f2c08a14f9ab55a3a
Gugo-le <rngustmd0719@naver.com>
2024-08-07 21:37:53 +0900

    multiple value 반환하는 방법

--- BASIC/GO/main.go
+++ BASIC/GO/main.go
@@ -1,11 +1,16 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func lenAndUpper(name string) (int, string) {
 	return len(name), strings.ToUpper(name)
 }
 
 func main() {
+	totalLength, upperName := lenAndUpper("kevin")
+	fmt.Println(totalLength, upperName)
 
 }
