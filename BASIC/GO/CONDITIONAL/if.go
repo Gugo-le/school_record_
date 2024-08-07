257c7d5aa454c08a60424a876e426b262e2748f5
Gugo-le <rngustmd0719@naver.com>
2024-08-07 22:26:12 +0900

    switch

--- BASIC/GO/CONDITIONAL/if.go
+++ BASIC/GO/CONDITIONAL/if.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 { // variable expression
+	switch age {
+	case 10:
 		return false
+	case 18:
+		return true
 	}
-	return true
-
+	return false
 }
 
 func main() {
