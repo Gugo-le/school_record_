a6f471af0e35fefce3e4d0f47606a6b1e5634344
Gugo-le <rngustmd0719@naver.com>
2024-08-07 23:13:35 +0900

    More Intuitive

--- BASIC/GO/DATASTRUCTURE/struct.go
+++ BASIC/GO/DATASTRUCTURE/struct.go
@@ -10,6 +10,6 @@ type person struct {
 
 func main() {
 	favFood := []string{"떡볶이", "집밥"}
-	kevin := person{"kevin", 18, favFood}
+	kevin := person{name: "kevin", age: 18, favFood: favFood}
 	fmt.Println(kevin)
 }
