ddd42abfd13daf3122594dcad200b5e2a0d5c267
Gugo-le <rngustmd0719@naver.com>
2024-08-08 20:06:36 +0900

    원리

---
+++ 1학년/동아리/BLOCKCHAIN/main.py
@@ -0,0 +1,12 @@
+'''
+Bansong Coin (BC)
+
+t1: Anna sends Bob 2BC
+t2: Bob sends Daniel 4.3 BC
+t3: Mark sends Charlie 3.2 BC
+
+SHA256
+B1("AAA", t1, t2, t3) -> 76fd89, B2("76fd89", t4, t5, t6) -> 8923ff, B3(8923ff, t7)
+
+BansongHash()
+'''
