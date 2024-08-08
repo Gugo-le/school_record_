238a080f702d63b7dc9b7531e3a701ae72260f81
Gugo-le <rngustmd0719@naver.com>
2024-08-08 22:16:16 +0900

    블록체인 초기화, 거래 생성, 채굴

--- 1학년/동아리/BLOCKCHAIN/blockchain/BS.py
+++ 1학년/동아리/BLOCKCHAIN/blockchain/BS.py
@@ -44,4 +44,44 @@ class Blockchain:
             {"from": None, "to": miner_address, "amount": self.mining_reward}
         ]
 
-    
+    def proof_of_work(self, block):
+        while not block.hash.startswith('0' * self.difficulty):
+            block.nonce += 1
+            block.hash = block.calculate_hash()
+        return block
+
+    def create_transaction(self, transaction):
+        self.pending_transactions.append(transaction)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
+
+        return True
+
+    def __str__(self):
+        chain_data = ""
+        for block in self.chain:
+            chain_data += str(block) + "\n"
+        return chain_data
+
+# 블록체인 초기화 및 거래 생성
+bansongcoin = Blockchain()
+
+# 새로운 거래 생성
+bansongcoin.create_transaction({"from": "Alice", "to": "Bob", "amount": 50})
+bansongcoin.create_transaction({"from": "Bob", "to": "Charlie", "amount": 25})
+
+# 채굴 (블록 생성)
+bansongcoin.mine_pending_transactions(miner_address="Miner1")
+
+print("Blockchain after mining a block:")
+print(bansongcoin)
+
