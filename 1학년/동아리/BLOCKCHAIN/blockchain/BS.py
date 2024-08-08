ee319bc2b97d951ea3428fa857088e084e9580ec
Gugo-le <rngustmd0719@naver.com>
2024-08-08 22:14:17 +0900

    블록체인 클래스 정의

--- 1학년/동아리/BLOCKCHAIN/blockchain/BS.py
+++ 1학년/동아리/BLOCKCHAIN/blockchain/BS.py
@@ -1,6 +1,7 @@
 import hashlib
 import time
 
+# 블록 클래스 정의
 class Block:
     def __init__(self, index, previous_hash, transactions, timestamp, nonce=0):
         self.index = index
@@ -18,3 +19,13 @@ class Block:
 
     def __str__(self):
         return f"Block #{self.index} [Previous Hash: {self.previous_hash}, Hash: {self.hash}, Transactions: {self.transactions}]"
+
+# 블록체인 클래스 정의
+class Blockchain:
+    def __init__(self, difficulty=2):
+        self.chain = [self.create_genesis_block()]
+        self.difficulty = difficulty
+        self.pending_transactions = []
+        self.mining_reward = 1
+
+    
